2a313608608cf9fd89a7b5a615aa28d2d4f4cec4
Vetrivel <95219637+2002-vetrivel@users.noreply.github.com>
2025-05-15 23:06:13 +0530

    Update package.js

--- package.js
+++ package.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const loadingGif = document.createElement('img');
     loadingGif.src = 'images/loading.gif';
-    loadingGif.alt = 'Loading...';
+    loadingGif.alt = 'Yes, I Know It’s Slow. My Coding Teacher Was YouTube 🤡';
     loadingGif.style.width = '130px';
     loadingGif.style.height = '130px';
 
